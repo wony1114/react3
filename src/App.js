3fd9c0a2c55f9cb982bc50cd4b9bdbdcf3178fb0
wony1114 <wony1114@nate.com>
2021-01-22 14:37:59 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
-/* import Login from './cmm/Login' */
-import BasicRouter from './tst/BasicRouter'
+/* import Blog from './bbs/pages/Blog' */
+/* import Album from './bbs/pages/Album' */
+/* import BasicRouter from './tst/BasicRouter' */
+/* import Checkout from './sts/pages/Checkout' */
+/* import Dashboard from './sts/pages/Dashboard' */
+/* import SignInSide from './sym/pages/SignInSide' */
+/* import Pricing from './uss/pages/Pricing' */
+/* import SiginUp from './uss/pages/StdnJoin' */
+/* import Home from './cmm/pages/Home' */
+import {Main} from './tst/pages'
 
 export default function App() {
   return (   
-    <BasicRouter/>
+    <Main/>
   )
 }
 
--- src/tst/BasicRouter.jsx
+++ src/tst/BasicRouter.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import Home from '../cmm/pages/Home'
+import About from './components/About'
+import Users from '../uss/pages/Users'
 
 export default function BasicRouter(){
-    return (<>
-        <div style={{border: '1px solid black', width: '400px', margin: '100 auto'}}>
+    return (<Router>
+        <div style={{border: '1px solid black', width: '100%', margin: '50px auto'}}>
             <nav>
                 <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Users</li>
+                    <li><Link to='/home'>Home</Link></li>
+                    <li><Link to='/about'>About</Link></li>
+                    <li><Link to='/users'>Users</Link></li>
                 </ul>
             </nav>
+            <Switch>
+                <Route path='/home' component={Home}></Route>
+                <Route path='/about' component={About}></Route>
+                <Route path='/users' component={Users}></Route>
+            </Switch>
         </div>
-    </>)
+    </Router>)
 }
+
+
+
+
